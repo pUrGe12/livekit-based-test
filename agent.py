f32822e17bc1b25f2cec941505def8084e21ac6e
pUrGe12 <helloitsme0218@gmail.com>
2025-12-24 12:02:58 +0530

    this finally works

--- agent.py
+++ agent.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 
 from livekit import agents
 from livekit.agents import AgentSession, Agent, RoomInputOptions
-# from livekit.plugins import noise_cancellation, silero
 from livekit.plugins.turn_detector.multilingual import MultilingualModel
 
 from tool import end_call, retrieve_transfer_status_data
@@ -13,10 +12,21 @@ load_dotenv(".env.local")
 class Assistant(Agent):
     def __init__(self) -> None:
         super().__init__(
-            instructions="""You are a helpful voice AI assistant.
-            You eagerly assist users with their questions by providing information from your extensive knowledge.
-            Your responses are concise, to the point, and without any complex formatting or punctuation including emojis, asterisks, or other symbols.
-            You are curious, friendly, and have a sense of humor.""",
+            instructions="""
+You are a wise app transfer status assistant. Be friendly to the user.
+
+If the user's question is about the status, delay, receipt, or whereabouts of a money transfer,
+call the retrieve_transfer_status_data tool with an appropriate search query.
+
+If the question is NOT about money transfer status AND about something else related to wise,
+say that you cannot answer the question and cut the call using end_call function.
+
+Do NOT answer transfer-status questions yourself, always use the tool.
+
+If the question is NOT about money transfer status AND also not something else related to wise, be polite in your replies but do not entertain such questions.
+
+Always answer in short sentences.
+            """,
             tools=[end_call, retrieve_transfer_status_data]
         )
 
@@ -39,15 +49,10 @@ async def entrypoint(ctx: agents.JobContext):
         instructions="Greet the user and offer your assistance."
     )
 
-
-
 if __name__ == "__main__":
     agents.cli.run_app(
         agents.WorkerOptions(
             entrypoint_fnc=entrypoint,
             agent_name="livekit-agent-3",
-            # Increase limits to 4GB or higher if needed
-            # memory_warn_mb=4000, 
-            # memory_limit_mb=8000, 
         )
     )
